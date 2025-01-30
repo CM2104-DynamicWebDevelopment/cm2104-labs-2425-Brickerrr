6cb01bcb6bb97a993677f2a0fefb6584c0a0d8d8
Brickerrr <finlaygray50@gmail.com>
2025-01-30 13:56:39 +0000

    final try

--- lab_11/server.js
+++ lab_11/server.js
@@ -22,9 +22,9 @@ app.get('/joke', function(req, res){
 });
 
 app.get('/add', function(req,res) {
-    var x = req.query.x;
-    var y = req.query.y;
-    res.send("X + Y =" + parseInt(x+y));
+    var x = parseInt(req.query.x);
+    var y = parseInt(req.query.y);
+    res.send("X + Y =" + (x+y));
 });
 
 app.listen(8080);
