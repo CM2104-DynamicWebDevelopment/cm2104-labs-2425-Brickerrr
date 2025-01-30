2648521e843032837fcbd3c4ef49e3b43422c4f0
Brickerrr <finlaygray50@gmail.com>
2025-01-30 13:54:18 +0000

    new route

--- lab_11/server.js
+++ lab_11/server.js
@@ -21,4 +21,10 @@ app.get('/joke', function(req, res){
     res.send(joke);
 });
 
+app.get('/add', function(req,res) {
+    var x = req.query.x;
+    var y = req.query.y;
+    res.send("X + Y =" + (x+y));
+});
+
 app.listen(8080);
