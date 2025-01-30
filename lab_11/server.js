967b50792fff945da3fb320af673535e3bb9f744
Brickerrr <finlaygray50@gmail.com>
2025-01-30 13:40:37 +0000

    new routes

--- lab_11/server.js
+++ lab_11/server.js
@@ -5,4 +5,14 @@ app.get('/', function(req, res){
     res.send("Hello World! by express");
 });
 
+//can use as many routes as you need to do different things
+
+app.get('/test', function(req, res){
+    res.send("this is route two, using /test");
+});
+
+app.get('/test2', function(req,res){
+    res.send("this is the final destination!");
+});
+
 app.listen(8080);
