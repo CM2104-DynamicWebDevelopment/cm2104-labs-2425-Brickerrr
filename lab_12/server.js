dceec33fc05d66b5bbcad3ef05dac091ce39074a
Brickerrr <finlaygray50@gmail.com>
2025-02-12 14:41:39 +0000

    debugging

--- lab_12/server.js
+++ lab_12/server.js
@@ -73,6 +73,7 @@ async function getTopTracks(artist, res) {
 
 app.get('/topSearch', function(req, res) {
     var topterm = req.query.topterm;
+    console.log(topterm);
     getTopTracks(topterm,res);
 })
 
