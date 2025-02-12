6a9a359a0e561747254fc86cde3fdd177f861296
Brickerrr <finlaygray50@gmail.com>
2025-02-12 14:42:45 +0000

    commit debug

--- lab_12/server.js
+++ lab_12/server.js
@@ -73,7 +73,7 @@ async function getTopTracks(artist, res) {
 
 app.get('/topSearch', function(req, res) {
     var topterm = req.query.topterm;
-    console.log(topterm);
+    console.log("term is" + topterm);
     getTopTracks(topterm,res);
 })
 
