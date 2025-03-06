32bdce89125122d009b4160156ab209c9f14af0e
Brickerr <finlaygray50@gmail.com>
2025-03-06 12:02:01 +0000

    bvd

--- lab_15/server.js
+++ lab_15/server.js
@@ -143,7 +143,7 @@ app.post('/dologin', function(req, res) {
 
 
 
-    if(result.login.password == pword){ req.session.loggedin = true; res.redirect('/') }
+    if(result.login.password == pword){ req.session.loggedin = true; res.redirect('/?uname='+uname) }
 
 
 
