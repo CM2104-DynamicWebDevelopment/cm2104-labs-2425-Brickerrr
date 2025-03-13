b870293413f20988611fa8f312cbb5d26a7dc798
Brickerrr <finlaygray50@gmail.com>
2025-03-13 11:37:51 +0000

    qwe

--- lab_16/public/scripts/main.js
+++ lab_16/public/scripts/main.js
@@ -1,6 +1,6 @@
 var socket = io();
 
 socket.on('chat message', function(msg) {
-    $('#messages').append("<li>"+ msg+"</>");
+    $('#messages').append("<li>"+ msg+"</li>");
     window.scrollTo(0, document.body.scrollHeight);
 })
